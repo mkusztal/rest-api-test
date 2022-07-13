1de29c7922d7a12e712370bf2f5cb69a084dbf71
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-07-13 18:54:49 +0300

    Change of order

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -8,12 +8,12 @@ router.route('/testimonials').get((req, res) => {
   res.json(db.testimonials);
 });
 
-router.route('/testimonials/:id').get((req, res) => {
-  res.json(db.testimonials.find((data) => data.id == req.params.id));
-});
-
 router.route('/testimonials/random').get((req, res) => {
   res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
+});
+
+router.route('/testimonials/:id').get((req, res) => {
+  res.json(db.testimonials.find((data) => data.id == req.params.id));
 });
 
 router.route('/testimonials').post((req, res) => {
