b7a067ea15ba7f0d4111f5953e5c20d4de18d93f
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-07-13 16:20:51 +0300

    Add import router

---
+++ routes/testimonials.routes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const uuid = require('uuid');
+const db = require('./../db.js');
+const router = express.Router();
+
+//endpoints
+router.route('/testimonials').get((req, res) => {
+  res.json(db.testimonials);
+});
+
+router.route('/testimonials/:id').get((req, res) => {
+  res.json(db.testimonials.find((data) => data.id == req.params.id));
+});
+
+router.route('/testimonials/random').get((req, res) => {
+  res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
+});
+
+router.route('/testimonials').post((req, res) => {
+  const { author, text } = req.body;
+  const id = uuid();
+
+  const newObject = {
+    id: id,
+    author: author,
+    text: text,
+  };
+
+  db.testimonials.push(newObject);
+  res.json({ message: 'Ok' });
+});
+
+// router.route('/testimonials/:id').put((req, res) => {
+//   const id = req.params.id;
+//   const findItem = db.testimonials.find((data) => data.id == req.params.id);
+//   const index = db.testimonials.indexOf(findItem);
+//   const { author, text } = req.body;
+//   const changeItem = {
+//     id: id,
+//     author: author,
+//     text: text,
+//   };
+
+//   res.json({ message: 'Ok' });
+// });
+
+router.route('/testimonials/:id').delete((req, res) => {
+  const item = db.testimonials.find((data) => data.id == req.param.id);
+  const index = db.testimonials.indexOf(item);
+
+  db.testimonials.splice(index, 1);
+  res.json({ message: 'Ok' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,68 +1,16 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
+//import endpoints
+const testimonials = require('./routes/testimonials.routes');
+
 //middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  {
-    id: 2,
-    author: 'Amanda Doe',
-    text: 'They really know how to make you happy.',
-  },
-];
-
-//endpoints
-app.get('testimonials', (req, res) => {
-  res.json(db);
-});
-
-app.get('testimonials/:id', (req, res) => {
-  res.json(db.find((data) => data.id == req.params.id));
-});
-
-app.get('testimonials/random', (req, res) => {
-  res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
-});
-
-app.post('testimonials', (req, res) => {
-  const { author, text } = req.body;
-  const id = uuid();
-
-  const newObject = {
-    id: id,
-    author: author,
-    text: text,
-  };
-
-  db.testimonials.push(newObject);
-  res.json({ message: 'Ok' });
-});
-
-// app.put('/testimonials/:id', (req, res) => {
-//   const id = req.params.id;
-//   const findItem = db.testimonials.find((data) => data.id == req.params.id);
-//   const index = db.testimonials.indexOf(findItem);
-//   const { author, text } = req.body;
-//   const changeItem = {
-//     id: id,
-//     author: author,
-//     text: text,
-//   };
-
-//   res.json({ message: 'Ok' });
-// });
-
-app.delete('/testimonials/:id', (req, res) => {
-  const item = db.testimonials.find((data) => data.id == req.param.id);
-  const index = db.testimonials.indexOf(item);
-
-  db.testimonials.splice(index, 1);
-  res.json({ message: 'Ok' });
-});
+app.use('/api', testimonials);
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
